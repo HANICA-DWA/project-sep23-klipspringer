936adc4bac1fc7ef41c4c7b9540e412e625357f7
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-11-13 13:43:44 +0100

    MUI

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <Grid columns={1} centered>
+      <Grid.Column textAlign="center">
+        <Image centered src="/images/bookicon.png" size="medium" />
+        <Header as="h1" size="huge">
+          Express your shelf
+        </Header>
+      </Grid.Column>
+    </Grid>
+  );
+}
