bad1d522a5dd88fc7d7570b6d3609ddc47aa554d
Joris van Lier <117820290+joris2003@users.noreply.github.com>
2023-11-13 10:37:13 +0100

    added mongoose and express session to server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const session = require('express-session')
 const app = express()
 
 const sessionParser = session({
@@ -9,3 +11,9 @@ const sessionParser = session({
 });
 
 app.use(sessionParser);
+
+const server = app.listen(async () => {
+    console.log("> connecting");
+    await mongoose.connect(`mongodb://127.0.0.1:27017/BKS`);
+    console.log("> connected");
+});
