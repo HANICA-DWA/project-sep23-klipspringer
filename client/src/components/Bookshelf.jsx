ee43b1f7dd908101ea9853cdb4188cb1c3189b19
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-11-14 15:51:15 +0100

    fix conflicts for #60 feature/add-book-on-profilepage

--- client/src/components/Bookshelf.jsx
+++ client/src/components/Bookshelf.jsx
@@ -1,4 +1,4 @@
-import {Typography, Card, Stack, CardMedia, Link} from "@mui/material";
+import {Typography, Card, Stack, CardMedia, Link, ImageList} from "@mui/material";
 
 export default function Bookshelf({ title, books }) {
       let loggedIn = true; // FIXME replace with actual login status
@@ -7,9 +7,10 @@ export default function Bookshelf({ title, books }) {
         <Stack direction="column" alignItems="center">
             <Typography variant="body1" fontWeight="600">{title}</Typography>
             <Stack direction="row" justifyContent="center" spacing={2}>
+                  <ImageList cols={3}>
                 {books.map(item =>
                     <Card key={item._id} style={{ width: "85px", height: "130px" }} >
-                        <Link href={"#"}><CardMedia height="130" component="img" image={item.url} alt="titel" /></Link>
+                        <Link href={"#"}><CardMedia height="130" component="img" image={item.cover_image} alt="titel" /></Link>
                     </Card>
                 )}
                   {loggedIn&&books.length<2?(
@@ -28,7 +29,7 @@ export default function Bookshelf({ title, books }) {
                         </Card>
                   ):""}
 
-
+                  </ImageList>
             </Stack>
             <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
         </Stack>
