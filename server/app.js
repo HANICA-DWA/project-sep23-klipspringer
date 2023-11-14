2ccab5805546fd7c5e2f91bc44488ea642a8c09f
Niels van Ee <nielsvanee@hotmail.com>
2023-11-14 11:40:29 +0100

    book retrieving van externe api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 
 import userRouter from './routes/userRouter.js'
+import bookRouter from './routes/bookRouter.js'
 
 const app = express()
 
@@ -17,7 +18,8 @@ const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3001;
 
 app.use(sessionParser);
-app.use("/user", userRouter)
+app.use("/user", userRouter);
+app.use("/book", bookRouter);
 
 const server = app.listen(port, host, async () => {
     console.log("> connecting");
