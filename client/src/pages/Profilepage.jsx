d1ae5296582cd2e1c7a7f49ca084e9d42dd04d08
roos1044 <ri.lodewijk@student.han.nl>
2023-11-14 09:46:03 +0100

    feedback verwerken

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -6,23 +6,23 @@ function Profilepage() {
     <Stack justifyContent="space-around" style={{ height: "100vh" }}>
       <Stack direction="column" alignItems="center">
         <Avatar alt="Voornaam Achternaam" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-        <Typography variant="body2">Voornaam Achternaam</Typography>
+        <Typography variant="body2" fontWeight="600">Voornaam Achternaam</Typography>
       </Stack>
 
       <Stack direction="column" alignItems="center">
-        <Typography variant="body1">My top 3 books</Typography>
+        <Typography variant="body1" fontWeight="600">My top 3 books</Typography>
         <Stack direction="row" justifyContent="center" spacing={2}>
           <Card style={{ width: "85px", height: "130px" }} >
-            <CardMedia component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
+            <CardMedia height="130" component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
           </Card>
           <Card style={{ width: "85px", height: "130px" }} >
-            <CardMedia component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
+            <CardMedia height="130" component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
           </Card>
           <Card style={{ width: "85px", height: "130px" }} >
-            <CardMedia component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
+            <CardMedia height="130" component="img" image="https://react.semantic-ui.com/images/wireframe/image.png" alt="titel" />
           </Card>
         </Stack>
-        <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg"></img>
+        <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
       </Stack>
 
       <Stack direction="column" alignItems="center">
