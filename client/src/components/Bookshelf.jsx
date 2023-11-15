2a1c103328cd053889bc7dc1d21a18e3ebfe0bd9
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-11-15 13:57:11 +0100

    commit refacture

--- client/src/components/Bookshelf.jsx
+++ client/src/components/Bookshelf.jsx
@@ -4,31 +4,46 @@ import {useEffect} from "react";
 export default function Bookshelf({ title, books }) {
       let loggedIn = true; // FIXME replace with actual login status
 
+      for (let i = books.length; i < 3; i++) {
+            placeholderBooks.push((
+                  <Card style={{ width: "85px", height: "130px" }} >
+                        <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>
+                  </Card>
+            ));
+      }
+
     return (
         <Stack direction="column" alignItems="center">
             <Typography variant="body1" fontWeight="600">{title}</Typography>
             <Stack direction="row" justifyContent="center" spacing={2}>
                   <ImageList cols={3}>
-                {books.map(item =>
+                {books.map(item =>(
                     <Card key={item._id} style={{ width: "85px", height: "130px" }} >
                         <Link href={"#"}><CardMedia height="130" component="img" image={item.cover_image} alt="titel" /></Link>
                     </Card>
-                )}
-                  {loggedIn&&books.length<2?(
-                        <>
-                              <Card style={{ width: "85px", height: "130px" }} >
-                                    <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>                                    <CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />
-                              </Card>
-                              <Card style={{ width: "85px", height: "130px" }} >
+                ))}
+                        {placeholderBooks.length!==0?
+                              placeholderBooks:
+                              (<Card style={{ width: "85px", height: "130px" }} >
                                     <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>
-                              </Card>
-                        </>
-                  ):""}
-                  {loggedIn&&books.length>1?(
-                        <Card style={{ width: "85px", height: "130px" }} >
-                              <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>
-                        </Card>
-                  ):""}
+                              </Card>)
+                        }
+
+                  {/*{loggedIn&&books.length<2?(*/}
+                  {/*      <>*/}
+                  {/*            <Card style={{ width: "85px", height: "130px" }} >*/}
+                  {/*                  <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>                                    <CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />*/}
+                  {/*            </Card>*/}
+                  {/*            <Card style={{ width: "85px", height: "130px" }} >*/}
+                  {/*                  <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>*/}
+                  {/*            </Card>*/}
+                  {/*      </>*/}
+                  {/*):""}*/}
+                  {/*{loggedIn&&books.length>1?(*/}
+                  {/*      <Card style={{ width: "85px", height: "130px" }} >*/}
+                  {/*            <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>*/}
+                  {/*      </Card>*/}
+                  {/*):""}*/}
 
                   </ImageList>
             </Stack>
