1f6fc52f29709248d36e75ebd04578262f2673bb
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-11-15 14:41:10 +0100

    review refactoring

--- client/src/components/Bookshelf.jsx
+++ client/src/components/Bookshelf.jsx
@@ -1,54 +1,60 @@
-import {Typography, Card, Stack, CardMedia, ImageList, Link} from "@mui/material";
+import { Typography, Card, Stack, CardMedia, ImageList, Link } from "@mui/material";
 
 export default function Bookshelf({ title, books }) {
-      let loggedIn = true; // FIXME replace with actual login status
+  let loggedIn = false; // FIXME replace with actual login status
 
-      const placeholderBooks = [];
+  const placeholderBooks = [];
 
-      for (let i = books.length; i < 3; i++) {
-            placeholderBooks.push((
-                  <Card style={{ width: "85px", height: "130px" }} >
-                        <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>
-                  </Card>
-            ));
-      }
+  //TODO na ontwerp Rik dit refactoren
+  if (books.length === 0 && !loggedIn) {
+    placeholderBooks.push(<div></div>);
+    placeholderBooks.push(
+      <Typography variant="h5" order="2">
+        No books in this shelf
+      </Typography>
+    );
+  }
 
-    return (
-        <Stack direction="column" alignItems="center">
-            <Typography gutterBottom variant="h5" fontWeight="800">{title}</Typography>
-            <Stack direction="row" justifyContent="center" spacing={2}>
-                  <ImageList cols={3}>
-                {books.map(item =>(
-                    <Card key={item._id} style={{ width: "85px", height: "130px" }} >
-                        <Link href={"#"}><CardMedia height="130" component="img" image={item.cover_image} alt="titel" /></Link>
-                    </Card>
-                ))}
-                        {loggedIn&&placeholderBooks.length!==0?
-                              placeholderBooks:
-                              (<Card style={{ width: "85px", height: "130px" }} >
-                                    <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>
-                              </Card>)
-                        }
+  for (let i = books.length; i < 3; i++) {
+    if (loggedIn) {
+      placeholderBooks.push(
+        <Card style={{ width: "85px", height: "130px" }}>
+          <Link href={"/search"}>
+            <CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />
+          </Link>
+        </Card>
+      );
+    }
+  }
 
-                  {/*{loggedIn&&books.length<2?(*/}
-                  {/*      <>*/}
-                  {/*            <Card style={{ width: "85px", height: "130px" }} >*/}
-                  {/*                  <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>                                    <CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />*/}
-                  {/*            </Card>*/}
-                  {/*            <Card style={{ width: "85px", height: "130px" }} >*/}
-                  {/*                  <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>*/}
-                  {/*            </Card>*/}
-                  {/*      </>*/}
-                  {/*):""}*/}
-                  {/*{loggedIn&&books.length>1?(*/}
-                  {/*      <Card style={{ width: "85px", height: "130px" }} >*/}
-                  {/*            <Link href={"/search"}><CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" /></Link>*/}
-                  {/*      </Card>*/}
-                  {/*):""}*/}
-
-                  </ImageList>
-            </Stack>
-            <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
-        </Stack>
-    )
+  return (
+    <Stack direction="column" alignItems="center">
+      <Typography gutterBottom variant="h5" fontWeight="800">
+        {title}
+      </Typography>
+      <Stack direction="row" justifyContent="center" spacing={2}>
+        <ImageList cols={3}>
+          {books.map((item) => (
+            <Card key={item._id} style={{ width: "85px", height: "130px" }}>
+              <Link href={"#"}>
+                <CardMedia height="130" component="img" image={item.cover_image} alt="titel" />
+              </Link>
+            </Card>
+          ))}
+          {placeholderBooks.length !== 0 ? (
+            placeholderBooks
+          ) : loggedIn ? (
+            <Card style={{ width: "85px", height: "130px" }}>
+              <Link href={"/search"}>
+                <CardMedia height="130" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />
+              </Link>
+            </Card>
+          ) : (
+            ""
+          )}
+        </ImageList>
+      </Stack>
+      <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
+    </Stack>
+  );
 }
