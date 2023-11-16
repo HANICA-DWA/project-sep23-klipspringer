abe549114764e3400fe729c5b00b67ca75f102d7
roos1044 <ri.lodewijk@student.han.nl>
2023-11-16 09:42:21 +0100

    edit knop toevoegen aan profielpagina

--- client/src/components/Bookshelf.jsx
+++ client/src/components/Bookshelf.jsx
@@ -1,6 +1,7 @@
 import { Typography, Card, Stack, CardMedia, ImageList, Link } from "@mui/material";
 import { useContext } from "react";
 import { LoggedInContext } from "../Contexts";
+import { Edit } from "@mui/icons-material";
 
 export default function Bookshelf({ title, books, hideAdding }) {
   const loggedIn = useContext(LoggedInContext).loggedIn;
@@ -56,7 +57,10 @@ export default function Bookshelf({ title, books, hideAdding }) {
           )}
         </ImageList>
       </Stack>
-      <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
+      <Stack direction="row">
+        <img style={{ width: "320px", height: "20px" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
+        <Edit/>
+      </Stack>
     </Stack>
   );
 }
