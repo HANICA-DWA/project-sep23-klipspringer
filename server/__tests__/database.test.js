0bb0bfb8d2c6039604831cf08856f80266150cd5
Niels van Ee <nielsvanee@hotmail.com>
2023-11-17 13:30:14 +0100

    added testing capability

---
+++ server/__tests__/database.test.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import { test, before, after, beforeEach, describe } from 'node:test'
+import assert from 'node:assert'
+
+import mongoose from 'mongoose'
+
+import User from '../model/user.js'
+
+before(async () => {
+    await mongoose.connect(`mongodb://127.0.0.1:27017/TestBKS`);
+})
+
+after(async () => {
+    await mongoose.disconnect();
+})
+
+describe('Bookcase Tests', () => {
+    beforeEach(async () => {
+        await User.deleteMany();
+        await User.create({
+            _id: "janwillem",
+            name: "Jan Willem",
+        }
+        );
+    })
+
+    test('AddingToBookcase', async () => {
+        let user = await User.findById('janwillem');
+        await user.addToBookcase([{_id: '123', cover_image: "image_url"}]);
+        await user.save()
+        let test_user = await User.findById('janwillem').lean();
+        assert.deepEqual(test_user.bookcase, [{_id: '123', cover_image: "image_url"}]);
+    })
+})
