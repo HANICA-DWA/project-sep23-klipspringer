d11566619e9d1e9a3c00c78598afb0342d2426cf
roos1044 <ri.lodewijk@student.han.nl>
2023-11-21 11:30:37 +0100

    testen maken

--- server/__tests__/function.test.js
+++ server/__tests__/function.test.js
@@ -1,28 +1,71 @@
-import { it, test, before, after, beforeEach, describe } from 'node:test'
+import { it, test, before, after, beforeEach, describe, afterEach } from 'node:test'
 import assert from 'node:assert';
 import { createError } from "../functions/errorCreation.js";
+import { getUserByUsername } from '../functions/users.js';
+import mongoose from 'mongoose'
+import User from '../model/user.js'
 import request from 'supertest';
 import app from '../app.js';
 
-// TODO createError
-describe('createError function', () => {
-    it('should create an error with default message and status', () => {
-      const defaultError = createError();
-      assert.strictEqual(defaultError.message, 'Something went wrong');
-      assert.strictEqual(defaultError.status, 500);
+
+describe("connection", () => {
+
+
+    before(async () => {
+        await mongoose.connect(`mongodb://127.0.0.1:27017/TestBKS`)
+
+    })
+
+    after(async () => {
+        await mongoose.disconnect();
+    })
+
+    // TODO createError
+    describe('createError function', () => {
+        it('should create an error with default message and status', () => {
+            const defaultError = createError();
+            assert.strictEqual(defaultError.message, 'Something went wrong');
+            assert.strictEqual(defaultError.status, 500);
+        });
+
+        it('should create an error with custom message and status', () => {
+            const customError = createError('Custom error message', 404);
+            assert.strictEqual(customError.message, 'Custom error message');
+            assert.strictEqual(customError.status, 404);
+        });
     });
-  
-    it('should create an error with custom message and status', () => {
-      const customError = createError('Custom error message', 404);
-      assert.strictEqual(customError.message, 'Custom error message');
-      assert.strictEqual(customError.status, 404);
-    });
-  });
 
-// TODO getUserByUsername
+    // TODO getUserByUsername
+    describe('getUserByUsername function', () => {
+        beforeEach(async () => {
+            await User.deleteMany();
+            await User.create({
+                _id: "janwillem",
+                name: "Jan Willem",
+            });
+
+        })
+
+        it("Should get the user information", async () => {
+            const username = "janwillem"
+
+            const userInfo = await getUserByUsername(username)
+            assert.strictEqual(userInfo._id, username)
+        })
+
+        it("Should get null", async () => {
+            const username = "doesnotExist"
+
+            const userInfo = await getUserByUsername(username)
+            assert.strictEqual(userInfo, null)
+        })
+
+    })
 
 
-// TODO getUserBySSOId
+    // TODO getUserBySSOId
 
 
-// TODO getUniqueId
+    // TODO getUniqueId
+
+})
