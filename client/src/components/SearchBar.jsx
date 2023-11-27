ea8ca957027a10af4e9d070a0186ab04ecc501a1
Niels van Ee <nielsvanee@hotmail.com>
2023-11-27 15:01:31 +0100

    small fix

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -34,7 +34,7 @@ export default function SearchBar({ onAdd }) {
       }
     }
 
-    return () => clearTimeout(timeoutId)
+    //return () => clearTimeout(timeoutId)
 
   }, [isOnCooldown, searchText])
 
