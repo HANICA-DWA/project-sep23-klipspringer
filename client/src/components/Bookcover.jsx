6accee2853a46596637101aab4a4c611fbddf179
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-11-28 14:26:21 +0100

    Optimizing book image fetching

--- client/src/components/Bookcover.jsx
+++ client/src/components/Bookcover.jsx
@@ -3,22 +3,23 @@ import { ImageNotSupported } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 
 export default function Bookcover({ isbn, cover_image }) {
-  const [imageSrc, setImageSrc] = useState("");
+  const [imageSrc, setImageSrc] = useState(`http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg?default=false`);
 
-  useEffect(() => {
-    const fetchImageSrc = async () => {
-      const response = await fetch(`http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg?default=false`);
-      if (response.status === 404) {
-        if (cover_image) setImageSrc(cover_image);
-      } else {
-        setImageSrc(`https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`);
-      }
-    };
-    fetchImageSrc();
-  }, [isbn, cover_image]);
+  const handleImageError = () => {
+    if (cover_image) {
+      setImageSrc(cover_image);
+    } else {
+      setImageSrc(null);
+    }
+  };
 
   return imageSrc ? (
-    <img style={{ borderRadius: "5px", width: "auto", maxWidth: "100%", height: "100%", objectFit: "cover" }} src={imageSrc} alt={isbn} />
+    <img
+      style={{ borderRadius: "5px", width: "auto", maxWidth: "100%", height: "100%", objectFit: "cover" }}
+      src={imageSrc}
+      onError={handleImageError}
+      alt={isbn}
+    />
   ) : (
     <Stack height="100%" width="auto" justifyContent="center" alignItems="center" sx={{ border: "1px solid black", borderRadius: "5px" }}>
       <Stack alignItems="center" justifyContent="center">
