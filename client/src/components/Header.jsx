22840f99b16826ef72d9c07e801686b3b36f17f3
roos1044 <ri.lodewijk@student.han.nl>
2023-12-11 14:22:57 +0100

    Kleine aanpassing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,16 @@
-import {useContext} from "react";
-import {LoggedInContext} from "../Contexts";
-import {Box, Stack} from "@mui/material";
-import {ArrowBackIosNew} from "@mui/icons-material";
+import { useContext } from "react";
+import { LoggedInContext } from "../Contexts";
+import { Box, Stack } from "@mui/material";
+import { ArrowBackIosNew } from "@mui/icons-material";
 import ProfileLink from "./ProfileLink";
-import {useAlert} from "../hooks/useAlert";
-import {useNavigate, useParams} from "react-router-dom";
-import {HamburgerMenu} from "./HamburgerMenu.jsx";
+import { useAlert } from "../hooks/useAlert";
+import { useNavigate, useParams } from "react-router-dom";
+import { HamburgerMenu } from "./HamburgerMenu.jsx";
 
 
-export default function Header({setLoggedIn, shareButton, backButton}) {
-    const {loggedIn, username} = useContext(LoggedInContext);
-    const {userName} = useParams();
+export default function Header({ setLoggedIn, shareButton, backButton }) {
+    const { loggedIn, username } = useContext(LoggedInContext);
+    const { userName } = useParams();
     const navigate = useNavigate();
 
     const [setShareLinkAlertOn, shareLinkAlert] = useAlert("Link copied to clipboard!");
@@ -18,25 +18,23 @@ export default function Header({setLoggedIn, shareButton, backButton}) {
     return (
         <>
             <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
-                {backButton ? (
-                    <Box sx={{flex: "1 1 0px", width: "0px", textAlign: "start"}}>
-                        <ArrowBackIosNew sx={{margin: "20px"}} onClick={() => navigate(-1)}/>
+                <Box sx={{ margin: "10px" }}>
+                    <Stack>
+                        <HamburgerMenu setLoggedIn={setLoggedIn} />
+                        {backButton ? (
+                            <ArrowBackIosNew sx={{marginTop: "5px"}} onClick={() => navigate(-1)} />
+                        ) : null}
+                    </Stack>
+                </Box>
+                {loggedIn && username === userName && shareButton ? (
+                    <Box>
+                        <ProfileLink alert={setShareLinkAlertOn} />
                     </Box>
-                ) : loggedIn && username === userName && shareButton ? (
-                    <Box sx={{flex: "1 1 0px", width: "0px", textAlign: "start"}}>
-                        <ProfileLink alert={setShareLinkAlertOn}/>
-                    </Box>
-                ) : (
-                    <Box sx={{flex: "1 1 0px"}}>
-                        <div></div>
-                    </Box>
-                )}
-                <Box sx={{flex: "1 1 0px", width: "0px", textAlign: "center"}}>
+                ) : null
+                }
+                {/* <Box sx={{ flex: "1 1 0px", width: "0px", textAlign: "center" }}>
 
-                </Box>
-                <Box sx={{flex: "1 1 0px", width: "0px", textAlign: "end"}}>
-                    <HamburgerMenu setLoggedIn={setLoggedIn}/>
-                </Box>
+                </Box> */}
             </Stack>
             {shareLinkAlert}
         </>
