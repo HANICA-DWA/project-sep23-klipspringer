79de69397ee1185726c136d4107db95b4be7cf83
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-12-11 13:45:56 +0100

    backend samenvatting ophalen

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -1,33 +1,55 @@
 import express from "express";
 
 import Book from "../model/book.js";
-import { createError } from "../functions/errorCreation.js";
+import {createError} from "../functions/errorCreation.js";
 
 const apiUrl = "https://openlibrary.org";
 
 const router = express.Router();
 
 router.get("/:id", (req, res, next) => {
-  Book.findById(req.params.id).then((book) => {
-    if (book === null) {
-      fetch(apiUrl + "/isbn/" + req.params.id + ".json")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          const cover_url = `https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg`;
-          const newBook = { _id: req.params.id, cover_image: cover_url, title: data.title, authors: [] };
-          Book.create(newBook);
-          res.send(newBook);
-        })
-        .catch((err) => {
-          const error = createError("Book not in external API", 400);
-          next(error);
-        });
-    } else {
-      res.send(book);
-    }
-  });
+	Book.findById(req.params.id).then((book) => {
+		if (book === null) {
+			fetch(apiUrl + "/isbn/" + req.params.id + ".json")
+				.then((res) => {
+					return res.json();
+				})
+				.then((data) => {
+					const cover_url = `https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg`;
+					const newBook = {_id: req.params.id, cover_image: cover_url, title: data.title, authors: []};
+					Book.create(newBook);
+					res.send(newBook);
+				})
+				.catch((err) => {
+					const error = createError("Book not in external API", 400);
+					next(error);
+				});
+		} else {
+			res.send(book);
+		}
+	});
 });
 
-export default router;
+router.get("/works/:id", (req, res, next) => {
+	fetch(apiUrl + "/isbn/" + req.params.id + ".json")
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+				fetch(apiUrl + data.works[0].key + ".json")
+					.then(res => res.json())
+					.then((data) => {
+						console.log(data);
+						res.send(data);
+					})
+					.catch((err) => {
+						const error = createError("Work not in external API", 400);
+						next(error);
+					})
+			}).
+			catch((err) => {
+				const error = createError("Book not in external API", 400);
+			});
+		});
+
+	export default router;
