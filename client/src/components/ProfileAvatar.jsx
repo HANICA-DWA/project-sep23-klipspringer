7e0fb3d56c0cb805acec58f3f4df064884d1075f
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-12-13 13:38:05 +0100

    prevent image caching

--- client/src/components/ProfileAvatar.jsx
+++ client/src/components/ProfileAvatar.jsx
@@ -1,9 +1,9 @@
 import { Avatar, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export default function ProfileAvatar({ alt, image, handle, clickable, border = true, onLoad }) {
+export default function ProfileAvatar({ alt, image, handle, clickable, border = true, onLoad, noCache = true }) {
   const link = handle ? `/${handle}` : "";
-  const imageLink = getAvatarUrl(image);
+  const imageLink = getAvatarUrl(image, noCache);
   const borderObject = border ? { padding: "3px", border: "1px solid grey", borderRadius: "50px" } : {};
   return (
     <Box sx={borderObject}>
@@ -30,12 +30,16 @@ export default function ProfileAvatar({ alt, image, handle, clickable, border = 
   );
 }
 
-function getAvatarUrl(stringUrl) {
+function getAvatarUrl(stringUrl, noCache) {
   if (stringUrl) {
     try {
-      return new URL(stringUrl);
+      const url = new URL(stringUrl);
+      if (noCache) url.searchParams.set("cacheBuster", new Date().getTime());
+      return url;
     } catch (e) {
-      return new URL(stringUrl, import.meta.env.VITE_BACKEND_HOST);
+      const url = new URL(stringUrl, import.meta.env.VITE_BACKEND_HOST);
+      if (noCache) url.searchParams.set("cacheBuster", new Date().getTime());
+      return url;
     }
   }
 }
