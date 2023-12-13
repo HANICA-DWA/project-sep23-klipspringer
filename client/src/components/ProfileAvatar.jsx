ac1b3445f9fe80e9ee4a8d8de576da998d7b8f01
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-12-13 10:08:02 +0100

    avatar on find

--- client/src/components/ProfileAvatar.jsx
+++ client/src/components/ProfileAvatar.jsx
@@ -1,17 +1,30 @@
 import { Avatar, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export default function ProfileAvatar({ alt, image, handle, clickable }) {
+export default function ProfileAvatar({ alt, image, handle, clickable, border = true, onLoad }) {
   const link = handle ? `/${handle}` : "";
   const imageLink = getAvatarUrl(image);
+  const borderObject = border ? { padding: "3px", border: "1px solid grey", borderRadius: "50px" } : {};
   return (
-    <Box sx={{ padding: "3px", border: "1px solid grey", borderRadius: "50px" }}>
+    <Box sx={borderObject}>
       {clickable ? (
         <Link to={link} style={{ textDecoration: "none" }}>
-          <Avatar sx={{ width: 56, height: 56 }} alt={alt} src={imageLink && imageLink.href} imgProps={{ referrerPolicy: "no-referrer" }} />
+          <Avatar
+            sx={{ width: 56, height: 56 }}
+            alt={alt}
+            src={imageLink && imageLink.href}
+            imgProps={{ referrerPolicy: "no-referrer" }}
+            onLoad={() => onLoad && onLoad()}
+          />
         </Link>
       ) : (
-        <Avatar sx={{ width: 56, height: 56 }} alt={alt} src={imageLink && imageLink.href} imgProps={{ referrerPolicy: "no-referrer" }} />
+        <Avatar
+          sx={{ width: 56, height: 56 }}
+          alt={alt}
+          src={imageLink && imageLink.href}
+          imgProps={{ referrerPolicy: "no-referrer" }}
+          onLoad={() => onLoad && onLoad()}
+        />
       )}
     </Box>
   );
