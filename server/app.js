65ffd6ee6dcec50e5a65ed22e4c3772e9c4ec8dd
YannickvandenBerg <MY.vandenBerg@student.han.nl>
2023-12-13 15:09:59 +0100

    first websocketserver setup

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import session from "express-session";
 import cors from "cors";
+import http from "http";
+import { WebSocketServer } from "ws";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -13,6 +15,8 @@ import multer from "multer";
 import genreRouter from "./routes/genreRouter.js";
 
 const app = express();
+const httpServer = http.createServer(app);
+const wsServer = new WebSocketServer({ noServer: true });
 
 const sessionParser = session({
   saveUninitialized: true,
@@ -51,7 +55,25 @@ app.use((err, req, res, next) => {
 
 export default app;
 
-export const server = app.listen(port, host, async () => {
+//handle upgrades from http requests to websocket
+httpServer.on("upgrade", (request, socket, head) => {
+  sessionParser(request, {}, () => {
+    //TODO check if session before continuing
+    wsServer.handleUpgrade(request, socket, head, (ws) => {
+      wsServer.emit("connection", ws, request);
+    });
+  });
+});
+
+wsServer.broadcastToFollowers = (ws, data) => {};
+
+wsServer.on("connection", (ws, request) => {
+  //TODO array or something from "following" to ws object, so broadcastToFollowers can use this array to find followers
+  //and then also own _id (handle) to ws object
+  ws.on("message", (data) => {});
+});
+
+export const server = httpServer.listen(port, host, async () => {
   console.log("> connecting");
   if (process.env.NODE_ENV === "test") {
     console.log("Connecting to test db");
