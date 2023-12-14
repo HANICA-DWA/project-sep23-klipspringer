8b04aa9284860cce77fcdc2dbd49a8c16c88dd3c
roos1044 <ri.lodewijk@student.han.nl>
2023-12-14 10:05:10 +0100

    book namen en auteur onder plank
    nog niet af

--- client/src/components/Bookshelf.jsx
+++ client/src/components/Bookshelf.jsx
@@ -8,7 +8,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { useAlert } from "../hooks/useAlert";
 import Bookcover from "./Bookcover";
 
-export default function Bookshelf({ id, title, books = [], hideAdding, user, unclickable, onDelete, edit, onBookDelete, nrOfColums = 3 }) {
+export default function Bookshelf({ id, title, books = [], hideAdding, user, unclickable, onDelete, edit, onBookDelete, nrOfColums = 3, placeholder }) {
   const { loggedIn, username } = useContext(LoggedInContext);
   const [errMessage, setErrMessage] = useState("");
   const [showAlert, alertComponent] = useAlert(errMessage, 3000, "warning");
@@ -43,85 +43,102 @@ export default function Bookshelf({ id, title, books = [], hideAdding, user, unc
   }
 
   return (
-    <Stack direction="column" alignItems="center">
-      {dialog}
-      {alertComponent}
-      <Typography gutterBottom variant="h5" fontWeight="800" sx={{ overflowWrap: "anywhere", maxWidth: "100%", textAlign: "center" }}>
-        {title}
-      </Typography>
-      {unclickable ? null : loggedIn && username === user && !edit ? (
-        <>
-          <IconButton
-            onClick={() => {
-              navigate(`/${username}/${id}/edit`);
-            }}
-          >
-            <Edit />
-          </IconButton>
-          {id !== "top_three" ? (
+    <>
+      <Stack direction="column" alignItems="center">
+        {dialog}
+        {alertComponent}
+        <Typography gutterBottom variant="h5" fontWeight="800" sx={{ overflowWrap: "anywhere", maxWidth: "100%", textAlign: "center" }}>
+          {title}
+        </Typography>
+        {unclickable ? null : loggedIn && username === user && !edit ? (
+          <>
             <IconButton
               onClick={() => {
-                setDialogOpen();
+                navigate(`/${username}/${id}/edit`);
               }}
             >
-              <Delete />
+              <Edit />
             </IconButton>
-          ) : null}
-        </>
-      ) : null}
-      <Stack direction="row" justifyContent="center" spacing={2}>
-        <ImageList cols={nrOfColums}>
-          {bookshelfBooks.map((item) => (
-            <Card key={item._id}>
-              <Box sx={{ width: "85px", height: "160px", display: "flex", justifyContent: "flex-end" }}>
-                {loggedIn && username === user && edit ? (
-                  <IconButton
-                    sx={{
-                      position: "absolute",
-                      marginTop: "-0.6rem",
-                      marginRight: "-0.6rem",
-                      bgcolor: "black",
-                      borderRadius: "50%",
-                      width: "1.7rem",
-                      height: "1.7rem",
-                      "&:hover": {
-                        bgcolor: "primary.main",
-                      },
-                    }}
-                    onClick={() => onBookDelete(item._id)}
-                  >
-                    <DeleteIcon
-                      sx={{
-                        position: "relative",
-                        color: "white",
-                        width: "1rem",
-                        height: "1rem",
-                      }}
-                    />
-                  </IconButton>
-                ) : null}
-                <Link to={unclickable ? null : `/book/${item._id}`} style={{ textDecoration: "none", color: "black" }}>
-                  <Bookcover isbn={item._id} cover_image={item.cover_image} />
+            {id !== "top_three" ? (
+              <IconButton
+                onClick={() => {
+                  setDialogOpen();
+                }}
+              >
+                <Delete />
+              </IconButton>
+            ) : null}
+          </>
+        ) : null}
+        <Stack direction="row" justifyContent="center" spacing={2}>
+          <ImageList cols={nrOfColums}>
+            {bookshelfBooks.map((item) => (
+              <>
+                <Card key={item._id}>
+                  <Box sx={{ width: "85px", height: "160px", display: "flex", justifyContent: "flex-end" }}>
+                    {loggedIn && username === user && edit ? (
+                      <IconButton
+                        sx={{
+                          position: "absolute",
+                          marginTop: "-0.6rem",
+                          marginRight: "-0.6rem",
+                          bgcolor: "black",
+                          borderRadius: "50%",
+                          width: "1.7rem",
+                          height: "1.7rem",
+                          "&:hover": {
+                            bgcolor: "primary.main",
+                          },
+                        }}
+                        onClick={() => onBookDelete(item._id)}
+                      >
+                        <DeleteIcon
+                          sx={{
+                            position: "relative",
+                            color: "white",
+                            width: "1rem",
+                            height: "1rem",
+                          }}
+                        />
+                      </IconButton>
+                    ) : null}
+                    <Link to={unclickable ? null : `/book/${item._id}`} style={{ textDecoration: "none", color: "black" }}>
+                      <Bookcover isbn={item._id} cover_image={item.cover_image} />
+                    </Link>
+                  </Box>
+                </Card>
+              </>
+            ))}
+            {placeholderBooks.length !== 0 ? (
+              placeholderBooks
+            ) : loggedIn && username === user && !hideAdding && id !== "top_three" ? (
+              <Card key={id} style={{ width: "85px", height: "160px" }}>
+                <Link to={`/${user}/${id}/add`}>
+                  <CardMedia shelf={id} height="160" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />
                 </Link>
-              </Box>
-            </Card>
+              </Card>
+            ) : (
+              ""
+            )}
+          </ImageList>
+        </Stack>
+        <Stack direction="row" sx={{ height: "20px", width: `${nrOfColums * 100}px`, maxWidth: "98vw", position: "relative", overflow: "hidden" }}>
+          <img style={{ bottom: 0, left: 0, top: 0, right: 0, position: "absolute", width: "100%" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
+        </Stack>
+        <Stack direction="row">
+          {bookshelfBooks.map((item) => (
+            <Stack margin="3px">
+              <Typography width="85px" variant="caption" fontWeight="600" >{item.title}</Typography>
+              <Typography width="85px" variant="caption" >{item.authors[0]}</Typography>
+            </Stack>
           ))}
-          {placeholderBooks.length !== 0 ? (
-            placeholderBooks
-          ) : loggedIn && username === user && !hideAdding && id !== "top_three" ? (
-            <Card key={id} style={{ width: "85px", height: "160px" }}>
-              <Link to={`/${user}/${id}/add`}>
-                <CardMedia shelf={id} height="160" component="img" image={"/images/Add-Icon.jpg"} alt="voeg een boek toe" />
-              </Link>
-            </Card>
-          ) : (
-            ""
-          )}
-        </ImageList>
+          {placeholderBooks !== 0 ? placeholderBooks.map((item) => (
+            <div style={{width: "85px"}}></div>
+          )) : null}
+          {loggedIn && username === user && !hideAdding && id !== "top_three" ?
+          <div style={{width: "85px"}}></div> : null}
+        </Stack>
       </Stack>
-      <Stack direction="row" sx={{ height: "20px", width: `${nrOfColums * 100}px`, maxWidth: "98vw", position: "relative", overflow: "hidden" }}>
-        <img style={{ bottom: 0, left: 0, top: 0, right: 0, position: "absolute", width: "100%" }} src="/images/bookshelf.jpg" alt="bookshelf"></img>
-      </Stack>
-    </Stack>
+    </>
   );
 }
