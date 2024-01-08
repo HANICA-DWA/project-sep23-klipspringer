f029ac309dec0d319907c6a17a909050b407d8d7
Niels van Ee <nielsvanee@hotmail.com>
2024-01-08 11:27:51 +0100

    fix voor import.meta

--- client/babel.config.cjs
+++ client/babel.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
     presets: [
         "@babel/preset-env",
-        ["@babel/preset-react", { runtime: "automatic" }]
+        ["@babel/preset-react", { runtime: "automatic" }],
+    ],
+    plugins: [
+        function () {
+            return {
+              visitor: {
+                MetaProperty(path) {
+                  path.replaceWithSourceString('process')
+                },
+              },
+            }
+          },
     ]
 }
