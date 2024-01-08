f584922cb14b4d3d4d6c92b5e1f4838ed52268e2
Niels van Ee <nielsvanee@hotmail.com>
2024-01-08 14:19:25 +0100

    Update Header.test.js

--- client/__tests__/components/Header.test.js
+++ client/__tests__/components/Header.test.js
@@ -5,20 +5,38 @@ import store from "../../src/redux/store/store"
 import { Provider } from "react-redux";
 import Router from "../../src/Router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { create } from "react-test-renderer";
+import { MemoryRouter } from "react-router";
+import '@testing-library/jest-dom'
 
 afterEach(cleanup);
 
 const navigate = jest.fn();
 
 it("Header should render without problems", () => {
-  render(
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GG_APP_ID}>
-        <Provider store={store}>
-    <Router>
-        <Header />
-    </Router>
-   
-    </Provider>
-    </GoogleOAuthProvider>
+    render(
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GG_APP_ID}>
+            <Provider store={store}>
+                <MemoryRouter>
+                    <Header />
+                </MemoryRouter>
+            </Provider>
+        </GoogleOAuthProvider>
     );
+});
+
+it("Should render all subcomponents", () => {
+    const { getByTestId } = render(
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GG_APP_ID}>
+            <Provider store={store}>
+                <MemoryRouter>
+                    <Header shareButton backButton />
+                </MemoryRouter>
+            </Provider>
+        </GoogleOAuthProvider>
+    );
+    
+    expect(getByTestId("MenuIcon")).toBeInTheDocument();
+    expect(getByTestId("ArrowBackIosNewIcon")).toBeInTheDocument();
+
 });
